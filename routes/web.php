afd999353232534cc06abc11df2723bfb13ab89a
Ken-Mwirigi <kenmwirigi7933@gmail.com>
2026-04-01 05:17:50 +0300

    Add database setup route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
-Route::get('/', function () {
-    return view('welcome');
+// The Secret Database Setup Route 
+Route::get('/setup-database', function () {
+    Artisan::call('migrate:fresh', [
+        '--force' => true,
+        '--seed' => true
+    ]);
+    return 'Database migrated and seeded successfully! You can go back to the dashboard now.';
 });
+
+// The Main Route 
+Route::get('/{any}', function () {
+    return view('welcome');
+})->where('any', '.*');
